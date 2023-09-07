3ba8c5d0441d853eea128d8f9bac7bb1128276eb
MatiBray <ma.bray@duocuc.cl>
2023-09-07 15:42:32 -0300

    animacion

--- teLLevoApp/src/app/pages/menu/menu.page.ts
+++ teLLevoApp/src/app/pages/menu/menu.page.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:proyecto-app-moviles-teLLevoApp/teLLevoApp/src/app/pages/menu/menu.page.ts
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { AnimationController, IonCard } from '@ionic/angular';
@@ -64,46 +63,3 @@ export class MenuPage implements OnInit {
   }
 
 }
-=======
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Menu } from 'src/app/models/menu';
-
-@Component({
-  selector: 'app-menu',
-  templateUrl: './menu.page.html',
-  styleUrls: ['./menu.page.scss'],
-})
-export class MenuPage implements OnInit {
-
-  menuArray:Menu[]=[]
-
-  constructor(private router:Router) { }
-
-  cargarMenu(){
-    this.menuArray.push(
-      {
-        id:1,
-        titulo:"solicitar",
-        url:"/solicitar",
-        icono:"chevron-forward-outline"
-      },
-      {
-        id:2,
-        titulo:"disponibilidad",
-        url:"/disponibilidad",
-        icono:"chevron-forward-outline",
-      }
-    );
-  }
-
-  ngOnInit() {
-    this.cargarMenu();
-  }
-
-  logOut(){
-    this.router.navigateByUrl("login");
-  }
-
-}
->>>>>>> 57e90fc8b186483917e6a9c230132e59a3590a0b:teLLevoApp/src/app/pages/menu/menu.page.ts
